0d843e8e153381c6f9247e62fc830263a108d0bc
Clayton Essex <clayessex@gmail.com>
2024-08-14 21:36:33 -0400

    add hue based clustering for sorting

--- namelist/buildlist.go
+++ namelist/buildlist.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"slices"
 	"strings"
 
 	"github.com/clayessex/colorproc/colors"
@@ -16,20 +17,28 @@ const (
 	OutputFilename = "colornames.go.out"
 )
 
+type Color struct {
+	name string
+	hex  colors.Hex
+	hsl  colors.Hsl
+	dist float64
+}
+
+// The choice of 84% yields 57.6 degrees of Hue and then 8% of each Saturation
+// and Luminance
+func distance(a, b colors.Hsl) float64 {
+	x := float64(a.H-b.H) / 360.0
+	y := float64(a.S - b.S)
+	z := float64(a.L - b.L)
+	return 0.84*x + 0.08*y + 0.08*z
+}
+
 func main() {
 	fout, err := os.Create(OutputFilename)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer fout.Close()
-
-	fout.WriteString(`
-const ColorNames = []struct{
-    hsl Hsl
-    hex Hex
-    name string
-}{
-`)
 
 	f, err := os.Open(ListFilename)
 	if err != nil {
@@ -42,6 +51,9 @@ const ColorNames = []struct{
 		log.Fatal(err)
 	}
 	defer z.Close()
+
+	colorNames := make([]Color, 0, 30000)
+	midpoint := colors.Hsl{180.0, 0.5, 0.5}
 
 	scanner := bufio.NewScanner(z)
 	maxLines := 20
@@ -64,11 +76,9 @@ const ColorNames = []struct{
 		if err != nil {
 			log.Printf("Line color is not parsable: [%s]", scanner.Text())
 		}
-		hslstring := hsl.F3String()
+		dist := distance(hsl, midpoint)
 
-		// TODO: create printers for go Hsl and Hex types
-
-		fmt.Fprintf(fout, "\thsl: %v, hex: %v, name: %s\n", hslstring, hex, name)
+		colorNames = append(colorNames, Color{name, hex, hsl, dist})
 
 		if maxLines--; maxLines == 0 {
 			log.Print("reached max lines")
@@ -77,6 +87,30 @@ const ColorNames = []struct{
 	}
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
+	}
+
+	slices.SortFunc(colorNames, func(a, b Color) int {
+		if a.dist < b.dist {
+			return -1
+		} else if a.dist > b.dist {
+			return 1
+		} else {
+			return 0
+		}
+	})
+
+	fout.WriteString(`
+const ColorNames = []struct{
+    name string
+    hex Hex
+    hsl Hsl
+}{
+`)
+
+	for _, v := range colorNames {
+		hslstring := fmt.Sprintf("Hsl{%f, %f, %f}", v.hsl.H, v.hsl.S, v.hsl.L)
+		fmt.Fprintf(fout, ` name: "%s", hex: "%v", hsl: %v, dist: %f`, v.name, v.hex, hslstring, v.dist)
+		fmt.Fprintf(fout, "\n")
 	}
 
 	fout.WriteString("}\n")
